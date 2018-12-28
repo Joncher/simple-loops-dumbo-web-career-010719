c778794160231f4ce06df6b7b2a9786f15346348
John Crespi Oncher <johnoncher@gmail.com>
2018-12-28 18:52:59 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,8 +22,8 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   while counter < number_of_times do
-    
-  
+    puts phrase
+  end
 end
 
 def until_iterator(number_of_times)
